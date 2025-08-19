99d4717a62bfe63324e94f7cdb009497a9644935
Vivian <vivian@limesli.me>
2025-08-18 20:19:34 -0700

    Link fortune hints to main.lua

--- worlds/tboirp/data/mod_template/main.lua
+++ worlds/tboirp/data/mod_template/main.lua
@@ -13,6 +13,8 @@ GREED_DONATION_LOCATION_COUNT = {{ greed_donation_location_count }}
 CONSUMABLE_LOCATION_COUNT = {{ consumable_location_count }}
 TARGET_BABY_CODES = {{ list_to_lua(target_baby_codes) }}
 
+ARCHIPELAGO_HINTS = require("fortune_hints.lua")
+
 local itemStates = require("item_states")
 
 local saveState = nil
